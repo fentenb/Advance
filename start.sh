f32d06ea661616956041ea6a8a41ff64f0edd39c
blackviva <110822720+blackviva@users.noreply.github.com>
2023-02-05 19:14:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/blackviva/Advance-EvaMaria /EvaMaria
+  git clone https://github.com/blackviva/Advance-EvaMaria /Advance-EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Advance-EvaMaria
 fi
-cd /EvaMaria
+cd /Advance-EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
