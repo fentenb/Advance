b0d9b7241f4813422779e9a487bad9b8add63697
blackviva <110822720+blackviva@users.noreply.github.com>
2023-12-04 17:41:53 +0300

    Add files via upload

--- broadcast.py
+++ broadcast.py
@@ -1,40 +1,45 @@
-
 from pyrogram import Client, filters
+from pyrogram.errors import InputUserDeactivated, UserIsBlocked, FloodWait
 import datetime
 import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import broadcast_messages
+#from utils import broadcast_messages
 import asyncio
+
         
-@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
-    users = await db.get_all_users()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    total = 0
-    successful = 0
-    blocked = 0
-    deleted = 0
-    unsuccessful = 0
-    
-    async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
-            successful += 1
-        elif pti == False:
-            if sh == "Blocked":
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS))
+async def _private_broadcast(bot, message):
+    if message.reply_to_message:
+        users = await db.get_all_users()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+        
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        async for user in users:
+            try:
+                await broadcast_msg.copy(user["id"])
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.value)
+                await broadcast_msg.copy(user["id"])
+                successful += 1
+            except UserIsBlocked:
+                await db.delete_user(user["id"])
                 blocked += 1
-            elif sh == "Deleted":
+            except InputUserDeactivated:
+                await db.delete_user(user["id"])
                 deleted += 1
-            elif sh == "Error":
+            except:
                 unsuccessful += 1
-        total += 1
+                pass
+            total += 1
         
-    status = f"""<b><u>Broadcast Completed</u>
+        status = f"""<b><u>Broadcast Completed</u>
 
 Total Users: <code>{total}</code>
 Successful: <code>{successful}</code>
@@ -42,43 +47,9 @@ Blocked Users: <code>{blocked}</code>
 Deleted Accounts: <code>{deleted}</code>
 Unsuccessful: <code>{unsuccessful}</code></b>"""
         
-    await sts.edit(status)    
-        
-        
-@Client.on_message(filters.command("group_broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/PremiumBotz
-async def gr_ou_pbr_oa_dc_a_st(bot, message):
-    chats = await db.get_all_chats()
-    b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    total = 0
-    successful = 0
-    blocked = 0
-    deleted = 0
-    unsuccessful = 0
-    
-    async for chat in chats:
-        pti, sh = await broadcast_messages(int(chat['id']), b_msg)
-        if pti:
-            successful += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked += 1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                unsuccessful += 1
-        total += 1
-        
-    status = f"""<b><u>Broadcast Completed</u>
+        return await pls_wait.edit(status)
 
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
-        
-    await sts.edit(status)            
-        
+    else:
+        msg = await message.reply("<code>Use this command as a replay to any telegram message with out any spaces.</code>")
+        await asyncio.sleep(8)
+        await msg.delete()
